97a70bc61aa015a78e872250ec719ae7723f3fe2
ironpup <christoph.ding@gmail.com>
2017-11-19 15:34:19 -0500

    Stub out actions.

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+export const testFunction = () => ({
+  return {
+    type: "HELLO_WORLD_REDUX",
+    text
+  }
+})
+
+function addToDo = () => ({
+
+})
+
+function removeToDo = () => ({
+
+})
+
+function completeToDo = () = ({
+
+})
+
+function makeProgressToDo = () = ({
+
+})
